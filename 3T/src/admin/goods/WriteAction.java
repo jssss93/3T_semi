2b18c7fd4df50c1efd63526650c26b2ced8947e1
jssss93 <cjs0533@naver.com>
2018-06-12 14:19:25 +0900

    20180612 14:28 -사진업로드 -상품 Write 업로드경로 수정

--- 3T/src/admin/goods/WriteAction.java
+++ 3T/src/admin/goods/WriteAction.java
@@ -55,7 +55,7 @@ public class WriteAction extends ActionSupport implements SessionAware {
 	private String goods_file_savname;
 	Calendar today = Calendar.getInstance();
 
-	private String fileUploadPath = "D:\\git\\3TT\\3T\\WebContent\\upload\\";
+	private String fileUploadPath = "C:\\upload\\";
 
 	private String file_orgname = "";
 	private String file_savname = "";
